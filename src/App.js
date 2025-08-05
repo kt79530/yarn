57bf8952512de452e24af64277fdfb9a7aab30d3
kt79530 <you@example.com>
2025-08-05 17:57:32 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Comp from './comp/Comp';
+//객체지향 언어는 불필요한 코드에 반복을 줄이고 언제든 재활용하세요.
+// Don't repeat your self
+//생명주기 컴포넌트의 생성, 변경, 소멸 과정을 뜻한다..
+//render(), constructor(), componentDidMount()
+//위의 태그들이 복잡하여 함수형 컴포넌트에 hook 등장
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <h1>리액트 시작하기</h1>
+   <p>sass 적용
+   <Comp/>
+   </p>
+   </>
   );
 }
 
---
+++ src/comp/Comp.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Comp extends Component{ 
+    //리액트 초기에 사용하던 클래스형 컴포넌트
+    render(){
+        return(
+            <h1>[This is Imported Component]</h1>
+        );
+    };
+}
+
+export default Comp;
+/*
+컴포넌트는 특정 코드뭉치를 다른 부분에 이식하거나 재사용하기 위해서 사용하는 코드블럭 단위
+클래스형 컴포넌트
+*/
